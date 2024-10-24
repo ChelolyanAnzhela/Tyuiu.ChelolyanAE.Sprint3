e7ba4cddc4195a40bd4b1bd9116a28c749d917d9
ChelolyanAnzhela <Tyuiu.Chelolyanae@outlook.com>
2024-10-24 21:29:40 +0500

    Task1

---
+++ Tyuiu.ChelolyanAE.Sprint3.Task1.V16/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.ChelolyanAE.Sprint3.Task1.V16.Lib;
+
+namespace Tyuiu.ChelolyanAE.Sprint3.Task1.V16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 |Выполнила: Челольян А. Е. | ИИПБ-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема:Оператор цикла while                                               *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнила: Челольян Анжела Евгеньевна | ИИПБ-24-2                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл while, которая вычисляет сумму        *");
+            Console.WriteLine("* ряда по формуле, при х = 0,7                                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double value = 0.7;
+            int startValue = 1;
+            int stopValue = 15;
+            double res = ds.GetSumSeries(value,startValue, stopValue);
+            Console.WriteLine($"Х:{value}");
+            Console.WriteLine($"Старт шага {startValue}");
+            Console.WriteLine($"Конец шага {stopValue}");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"Сумма ряда ={res}");
+            Console.ReadKey();
+        }
+    }
+}
