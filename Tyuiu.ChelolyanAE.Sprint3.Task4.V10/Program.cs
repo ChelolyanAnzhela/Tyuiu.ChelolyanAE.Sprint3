2ba6c5d0a347775acdd7f2bbc240056045bcfbc2
ChelolyanAnzhela <Tyuiu.Chelolyanae@outlook.com>
2024-11-01 22:39:09 +0500

    Task4

---
+++ Tyuiu.ChelolyanAE.Sprint3.Task4.V10/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.ChelolyanAE.Sprint3.Task4.V10.Lib;
+
+namespace Tyuiu.ChelolyanAE.Sprint3.Task4.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 |Выполнила: Челольян А. Е. | ИИПБ-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема:Оператор цикла continue и break в циклах                           *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнила: Челольян Анжела Евгеньевна | ИИПБ-24-2                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
+            Console.WriteLine("* функции y=(sin(x)-x/x).При x=0 пропустить значение. Полученные значения *");
+            Console.WriteLine("* Полученные значения перемножать.                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine($"Старт шага = {startValue}");
+            Console.WriteLine($"Конец шага = {stopValue}");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"Сумма ряда {ds.Calculate(startValue, stopValue)}");
+            Console.ReadKey();
+
+        }
+    }
+}
