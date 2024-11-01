0bb9dc283fd55ec0772350a9b7fa529d9f9edf9e
ChelolyanAnzhela <Tyuiu.Chelolyanae@outlook.com>
2024-11-01 23:18:18 +0500

    Task5

---
+++ Tyuiu.ChelolyanAE.Sprint3.Task5.V21/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.ChelolyanAE.Sprint3.Task5.V21.Lib;
+
+namespace Tyuiu.ChelolyanAE.Sprint3.Task5.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 |Выполнила: Челольян А. Е. | ИИПБ-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема:Вложенные циклы                                                    *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнила: Челольян Анжела Евгеньевна | ИИПБ-24-2                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* высчитать сумму сумм ряда (x^3*cos(k))+2. При x=2                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x = 2;
+            int startValue1 = 1;
+            int stopValue1 = 3;
+            int startValue2 = 1;
+            int stopValue2 = 10;
+            Console.WriteLine($"Старт шага первой суммы {startValue1}");
+            Console.WriteLine($"Конец шага первой суммы {stopValue1}");
+            Console.WriteLine($"Старт шага второй суммы {startValue2}");
+            Console.WriteLine($"Конец шага второй суммы {stopValue1}");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"Сумма сумм ряда = {ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2)}");
+            Console.ReadKey();
+        }
+    }
+}
