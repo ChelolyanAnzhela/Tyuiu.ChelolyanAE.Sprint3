969876c4948ef3182125cccf93ea523a6027f246
ChelolyanAnzhela <Tyuiu.Chelolyanae@outlook.com>
2024-11-02 14:09:04 +0500

    Task7

---
+++ Tyuiu.ChelolyanAE.Sprint3.Task7.V20/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.ChelolyanAE.Sprint3.Task7.V20.Lib;
+
+namespace Tyuiu.ChelolyanAE.Sprint3.Task7.V20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 |Выполнила: Челольян А. Е. | ИИПБ-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема:Обработка целочисленной информации                                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнила: Челольян Анжела Евгеньевна | ИИПБ-24-2                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая выводит таблицу значений функции            *");
+            Console.WriteLine("* F(x)=(sin(x)-2x/3x-1)+sin(x)-3x+2                                       *");
+            Console.WriteLine("* на заданном диапазоне [-5,5] с шагом 1.                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int startValue = -5;
+            int stopValue= 5;
+            Console.WriteLine($"Старт шага = {startValue}");
+            Console.WriteLine($"Конец шага = {stopValue}");
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+            double[] valueArray;
+            valueArray = new double[len];
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("+----------+-----------+");
+            Console.WriteLine("|    X     +   f(x)    |");
+            Console.WriteLine("+----------+-----------+");
+            for (int i = 0; i <= len-1 ; i++)
+            {
+                Console.WriteLine("|{0,5:d}     |  {1,6:f2}   |", startValue,valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+----------+-----------+");
+            Console.ReadKey();
+          
+
+        }
+    }
+}
